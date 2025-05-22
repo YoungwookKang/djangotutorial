8d4a8f751f4691a9ca284c06b738c5b47d1a9de1
Youngwook Kang <kyoungs0603@gmail.com>
2025-05-22 23:36:24 +0900

    Implement templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,30 @@
-from django.urls import path
+from django.urls import path # path 함수는 URL 패턴과 뷰 함수를 매핑하는 데 사용됩니다.
 
-from . import views
+from . import views # 현재 디렉토리(polls 앱)의 views.py 모듈을 가져옵니다. 여기에는 우리가 정의한 뷰 함수들이 있습니다.
 
+# app_name 변수는 URL 네임스페이스(namespace)를 설정합니다.
+# 이렇게 하면 다른 앱에서 동일한 URL 이름을 사용하더라도 구분할 수 있습니다.
+# 예를 들어, 템플릿에서 {% url 'polls:detail' question.id %} 와 같이 사용합니다.
+app_name = "polls"
+
+# urlpatterns 리스트는 이 앱(polls) 내의 URL 패턴들을 정의합니다.
+# Django는 요청된 URL과 이 패턴들을 순서대로 비교하여 일치하는 것을 찾습니다.
 urlpatterns = [
+  # ex: /polls/ (이 앱의 기본 URL)
+    # 첫 번째 인자: URL 패턴 문자열. 빈 문자열은 /polls/ 자체를 의미합니다 (mysite.urls에서 /polls/로 시작하는 요청이 이리로 오기 때문).
+    # 두 번째 인자: 이 URL 패턴과 일치할 때 호출될 뷰 함수 (views.py의 index 함수).
+    # name 인자: 이 URL 패턴에 이름을 부여합니다. 템플릿이나 코드에서 URL을 하드코딩하는 대신 이 이름을 사용할 수 있어 유연성이 높아집니다.
     path("", views.index, name="index"),
+    
+    # ex: /polls/5/ (숫자 ID를 가진 질문 상세 페이지)
+    # "<int:question_id>/": URL 경로의 일부를 캡처하여 뷰 함수에 인자로 전달합니다.
+    #   - int: 경로 변환기(path converter)로, 이 부분이 정수여야 함을 의미합니다.
+    #   - question_id: 캡처된 값에 대한 변수 이름. views.detail 함수의 question_id 매개변수로 전달됩니다.
+    path("<int:question_id>/", views.detail, name="detail"),
+    
+    # ex: /polls/5/results/ (특정 질문의 결과 페이지)
+    path("<int:question_id>/results/", views.results, name="results"),
+    
+    # ex: /polls/5/vote/ (특정 질문에 대한 투표 처리 URL)
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from django.template import loader
+from django.http import HttpResponse, Http404
 
-from django.http import HttpResponse
+from .models import Question, Choice
 
 # 장고에서의 views는 요청을 처리하는 함수들의 집합. 엔드포인트에 대한 요청을 처리하는 함수들을 작성하는 곳.
+
+# index 뷰 함수: /polls/ URL 요청을 처리합니다.
+# 가장 최근에 발행된 질문 5개를 가져와서 텍스트로 보여줍니다.
 def index(request):
-    print("index 호출됨")
-    input("아무키나 누르세요")
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Question.objects는 Question 모델의 모든 객체에 접근할 수 있는 매니저(manager)입니다. 각 장고 모델에는 objects라는 매니저가 있음.
+    # order_by("-pub_date")는 pub_date 필드를 기준으로 내림차순 정렬합니다 (최신순).
+    # [:5]는 정렬된 결과 중 처음 5개만 가져옵니다 (슬라이싱).
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    
+    # render 함수는 템플릿을 렌더링하고, 결과를 HttpResponse 객체로 반환합니다.
+    # 이 함수는 템플릿 경로와 컨텍스트 데이터를 인자로 받습니다.    
+    return render(request, "polls/index.html", context)
+# detail 뷰 함수: /polls/<question_id>/ URL 요청을 처리합니다.
+# URL에서 question_id 값을 받아서 해당 ID의 질문 내용을 보여줍니다 (현재는 플레이스홀더).
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+# results 뷰 함수: /polls/<question_id>/results/ URL 요청을 처리합니다.
+# 특정 질문의 결과를 보여줍니다 (현재는 플레이스홀더).
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# vote 뷰 함수: /polls/<question_id>/vote/ URL 요청을 처리합니다.
+# 특정 질문의 특정 선택지에 투표하는 기능을 처리합니다 (현재는 플레이스홀더).
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
